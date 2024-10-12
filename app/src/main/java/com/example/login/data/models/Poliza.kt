912607b6b601f7de1169be736e9e868a1db5ab0c
lukitasmois <lmoix85@gmail.com>
2024-10-12 17:23:04 -0300

    Agregue atributo id en los objetos Poliza

--- app/src/main/java/com/example/login/data/models/Poliza.kt
+++ app/src/main/java/com/example/login/data/models/Poliza.kt
@@ -1,6 +1,10 @@
 package com.example.login.data.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Poliza(
+    @SerializedName("_id")
+    val id: Int,
     val dominio: String
 
 )
