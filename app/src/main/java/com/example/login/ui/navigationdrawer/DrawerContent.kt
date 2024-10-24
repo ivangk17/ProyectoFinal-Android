f6e49b8148de8b5ff41d2c07c78c08849b5a64c8
Pehuenia <msaguero@gmail.com>
2024-10-24 17:36:38 -0300

    Se agregan items a DrawerContent.

--- app/src/main/java/com/example/login/ui/navigationdrawer/DrawerContent.kt
+++ app/src/main/java/com/example/login/ui/navigationdrawer/DrawerContent.kt
@@ -2,16 +2,23 @@ package com.example.login.ui.navigationdrawer
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DrawerState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.login.ui.viewmodels.navdrawerviewmodel.DrawerViewModel
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun DrawerContent(
@@ -29,7 +36,23 @@ fun DrawerContent(
             .padding(top = 12.dp), // Agregar padding superior
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        Text(text = "Contenido del drawer", modifier = Modifier.padding(start = 16.dp))
+        drawerItems.forEach { item ->
+            NavigationDrawerItem(
+                label = {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(imageVector = item.userIcon, contentDescription = item.text)
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(text = item.text)
+                    }
+                },
+                selected = item == selectedItem,
+                onClick = {
+                    onItemSelected(item)
+                    scope.launch { drawerState.close() }
+                },
+                modifier = Modifier.padding(horizontal = 10.dp),
+            )
+        }
 
     }
 }
