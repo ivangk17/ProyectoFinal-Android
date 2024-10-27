627e6a8005a84b7c49fcdcbfb4b957eadc7a0341
lukitasmois <lmoix85@gmail.com>
2024-10-27 15:30:15 -0300

    merge con nav drawer

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -4,23 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
-import com.example.login.data.network.services.GetStatus
-import com.example.login.data.network.RetrofitClient
 import com.example.login.navigation.AppNavigation
-import com.example.login.ui.viewmodels.LoadingViewModel
-import com.example.login.ui.viewmodels.MainActivityViewModel
 import com.example.login.ui.viewmodels.navdrawerviewmodel.DrawerViewModel
 import com.example.login.ui.viewmodels.navdrawerviewmodel.DrawerViewModelFactory
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: MainActivityViewModel by viewModels {
-        MainActivityViewModel.provideFactory(
-            GetStatus(RetrofitClient.apiService)
-        )
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/login/tokens/Token.kt
+++ app/src/main/java/com/example/login/tokens/Token.kt
@@ -4,5 +4,5 @@ object Token {
     //lateinit var token: String
 //TODO BORRAR ESTO
 
-    var token: String ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzAyMGQ2M2EyYmFiY2MwZWFhNWFjNTciLCJlbWFpbCI6ImNsaWVudGVAY2xpZW50ZS5jb20iLCJyb2xlIjoiYXNlZ3VyYWRvIiwiaWF0IjoxNzI5ODkzNDE4LCJleHAiOjE3Mjk4OTcwMTh9.J0QEaACjMUD0dI0f09RQzhQcOyMeCHL8dWFxBkr9SbU"
+    var token: String ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzE2NWY1Mjk5Mzk2MDFhZWI2MDgyZjYiLCJlbWFpbCI6InRlc3RjbGllbnRAdGVzdGNsaWVudC5jb20iLCJyb2xlIjoiYXNlZ3VyYWRvIiwiaWF0IjoxNzMwMDUxNTMyLCJleHAiOjE3MzAwNTUxMzJ9.MdW6BnEpyZXdwKi3wUiU3xEGvXq52z2g-mcQ9QjqdOQ"
 }
