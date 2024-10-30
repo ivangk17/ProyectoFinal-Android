62af932db436c9460db0b113388ea09410df340b
Pehuenia <msaguero@gmail.com>
2024-10-30 17:09:56 -0300

    listado-solicitudes: Corrección de detalles generales

--- app/src/main/java/com/example/login/ui/screens/solicitudes/SolicitudesScreen.kt
+++ app/src/main/java/com/example/login/ui/screens/solicitudes/SolicitudesScreen.kt
@@ -1,16 +1,16 @@
 package com.example.login.ui.screens.solicitudes
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -23,20 +23,19 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.login.R
 import com.example.login.data.models.solicitud.SolicitudSimplificada
-import com.example.login.navigation.Rutas
 import com.example.login.ui.navigationdrawer.NavDrawer
 import com.example.login.ui.viewmodels.navdrawerviewmodel.DrawerViewModel
 import com.example.login.ui.viewmodels.solicitudesviewmod.SolicitudesViewModel
 import kotlinx.coroutines.launch
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SolicitudesScreen(
     viewModel: SolicitudesViewModel,
@@ -71,7 +70,7 @@ fun SolicitudesScreen(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
-                    "Solicitudes",
+                    text = stringResource(R.string.Solicitudes_titulo),
                     fontSize = 30.sp,
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier.padding(top = 4.dp),
@@ -86,31 +85,47 @@ fun SolicitudesScreen(
 @Composable
 fun SolicitudesList(solicitudes: List<SolicitudSimplificada>, navController: NavHostController) {
 
-    Log.d("SolicitudesList", "Solicitudes size: ${solicitudes.size}")
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        Log.d("SolicitudesScreen", "Solicitudes size: ${solicitudes.size}")
-        items(solicitudes) { solicitud ->
-            Card(
-                modifier = Modifier
-                    .padding(15.dp)
-                    .fillMaxWidth()
-                    .clickable {
-                       // navController.navigate(Rutas.SolicitudDetalle.rutaConId(solicitud.id))
+
+    if (solicitudes.isEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .offset(y=-100.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = stringResource(R.string.Sin_solicitudes), fontSize = 25.sp,fontWeight = FontWeight.Bold, color = colorResource(id = R.color.texto_Secundario))
+        }
+    } else {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+
+            items(solicitudes) { solicitud ->
+                Card(
+                    modifier = Modifier
+                        .padding(15.dp)
+                        .fillMaxWidth()
+                        .clickable {
+                            // navController.navigate(Rutas.SolicitudDetalle.rutaConId(solicitud.id))
+
+                        }
+                ) {
+                    Column(modifier = Modifier.padding(8.dp)) {
+
+                        Text(text = stringResource(R.string.id_solicitud, solicitud.idSolicitud))
+                        Text(text = stringResource(R.string.estado, solicitud.estado))
+                        Text(
+                            text = stringResource(
+                                id = R.string.fecha_siniestro,
+                                solicitud.fechaOcurrencia
+                                    ?: stringResource(id = R.string.dato_no_disponible)
+                            )
+                        )
+
 
                     }
-            ) {
-                Column(modifier = Modifier.padding(8.dp)) {
-
-                    Text(text = "ID de la solicitud: ${solicitud.idSolicitud}")
-                    Text(text = "Estado: ${solicitud.estado}")
-                    Text(text = "Fecha del siniestro: ${solicitud.fechaOcurrencia ?: "Fecha no disponible"}")
-                    Text(text = "Datos del cliente asegurado:", modifier = Modifier.padding(top = 3.dp))
-                    Text(text = "ID: ${solicitud.idAsegurado ?: "No disponible"}")
-                    Text(text = "Apellido y nombre: ${solicitud.nombreAsegurado}")
-
-
                 }
             }
         }
+
     }
+
+
 }
