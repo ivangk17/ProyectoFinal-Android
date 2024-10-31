40f43f079a299086be833fbcb02df95b11462449
Pehuenia <msaguero@gmail.com>
2024-10-30 22:31:08 -0300

    listado-solicitudes: Se modulariza SolicitudesScreen.

---
+++ app/src/main/java/com/example/login/ui/screens/solicitudes/SolicitudesLista.kt
@@ -0,0 +1,53 @@
+package com.example.login.ui.screens.solicitudes
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.login.R
+import com.example.login.data.models.solicitud.SolicitudSimplificada
+
+@Composable
+fun SolicitudesLista(solicitudes: List<SolicitudSimplificada>, navController: NavHostController) {
+
+
+    if (solicitudes.isEmpty()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .offset(y = -100.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = stringResource(R.string.Sin_solicitudes),
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(id = R.color.texto_Secundario)
+            )
+        }
+    } else {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+
+            items(solicitudes) { solicitud ->
+                SolicitudCard(solicitud) {
+
+                }
+
+            }
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/example/login/ui/screens/solicitudes/SolicitudesScreen.kt
+++ app/src/main/java/com/example/login/ui/screens/solicitudes/SolicitudesScreen.kt
@@ -1,20 +1,11 @@
 package com.example.login.ui.screens.solicitudes
 
-import android.icu.text.CaseMap
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SliderDefaults.Thumb
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -25,9 +16,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -35,7 +23,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.login.R
-import com.example.login.data.models.solicitud.SolicitudSimplificada
 import com.example.login.ui.navigationdrawer.NavDrawer
 import com.example.login.ui.viewmodels.navdrawerviewmodel.DrawerViewModel
 import com.example.login.ui.viewmodels.solicitudesviewmod.SolicitudesViewModel
@@ -81,80 +68,11 @@ fun SolicitudesScreen(
                     fontSize = 30.sp,
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier.padding(bottom = 25.dp),
-                    color = colorResource(id = R.color.texto_principal)
-,                )
-                SolicitudesList(solicitudes, navController)
+                    color = colorResource(id = R.color.texto_principal),
+                )
+                SolicitudesLista(solicitudes, navController)
             }
         }
     }
 }
 
-@Composable
-fun SolicitudesList(solicitudes: List<SolicitudSimplificada>, navController: NavHostController) {
-
-
-    if (solicitudes.isEmpty()) {
-        Box(
-            modifier = Modifier.fillMaxSize()
-                .offset(y=-100.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = stringResource(R.string.Sin_solicitudes), fontSize = 25.sp,fontWeight = FontWeight.Bold, color = colorResource(id = R.color.texto_Secundario))
-        }
-    } else {
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-
-            items(solicitudes) { solicitud ->
-                Card(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .padding(horizontal = 16.dp)
-                        .fillMaxWidth()
-                        .clickable {
-                            // navController.navigate(Rutas.SolicitudDetalle.rutaConId(solicitud.id))
-
-                        }
-                ) {
-
-
-                    Box(
-                        modifier = Modifier
-                            .background(
-                                Brush.linearGradient(
-                                    colors = listOf(Color(0xFFD0CFCF), Color(0xFBCECECE)),
-                                    start = Offset(0f, 0f),
-                                    end = Offset(400f, 400f) // Ajusta esto para cambiar la dirección del gradiente
-                                )
-                            )
-                            .padding(16.dp) // Padding para el contenido de la tarjeta
-                    ) {
-
-                        Column(modifier = Modifier.padding(8.dp)) {
-
-
-
-                            Text(text = stringResource(R.string.estado, solicitud.estado))
-                            Text(
-                                text = stringResource(
-                                    id = R.string.fecha_siniestro,
-                                    solicitud.fechaOcurrencia
-                                        ?: stringResource(id = R.string.dato_no_disponible)
-                                )
-                            )
-
-
-
-                        }
-
-                    }
-
-
-
-                }
-            }
-        }
-
-    }
-
-
-}
