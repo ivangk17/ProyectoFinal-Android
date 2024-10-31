84399761c018bbe48f882ffe4c6e405c43d0d3f4
lukitasmois <lmoix85@gmail.com>
2024-10-31 15:26:16 -0300

    Cambio en la agp

--- app/src/main/java/com/example/login/tokens/Token.kt
+++ app/src/main/java/com/example/login/tokens/Token.kt
@@ -1,8 +1,7 @@
 package com.example.login.tokens
 
 object Token {
-    //lateinit var token: String
-//TODO BORRAR ESTO
+    lateinit var token: String
 
     //var token: String ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzE2NWY1Mjk5Mzk2MDFhZWI2MDgyZjYiLCJlbWFpbCI6InRlc3RjbGllbnRAdGVzdGNsaWVudC5jb20iLCJyb2xlIjoiYXNlZ3VyYWRvIiwiaWF0IjoxNzMwMDUxNTMyLCJleHAiOjE3MzAwNTUxMzJ9.MdW6BnEpyZXdwKi3wUiU3xEGvXq52z2g-mcQ9QjqdOQ"
 }
