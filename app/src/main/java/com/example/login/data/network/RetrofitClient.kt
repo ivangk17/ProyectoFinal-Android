11082d07f58ff095f657c127406e7b310e02a1e0
lukitasmois <lmoix85@gmail.com>
2024-10-31 14:19:04 -0300

    Agrego el interceptor para que el cliente pueda loguear desde la aplicacion

--- app/src/main/java/com/example/login/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/login/data/network/RetrofitClient.kt
@@ -1,5 +1,6 @@
 package com.example.login.data.network
 import com.example.login.data.network.Api
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,14 +9,19 @@ object RetrofitClient {
     private const val BASE_URL = "http://10.0.2.2:3000"
 
     // con dispositivo fisico
-    //private const val BASE_URL = "http:/192.168.1.7:3000/"
+    //private const val BASE_URL = "http:/192.168.0.85:3000/"
 
-    // Create the Retrofit instance
+
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(UserAgentInterceptor())
+        .build()
+
+
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create()) // Using Gson converter
         .build()
 
-    // Get the ApiService instance from Retrofit
     val apiService: Api = retrofit.create(Api::class.java)
 }
