55cf0aa1d2d789fb51536771f769b8f4340d3d6f
Pehuenia <msaguero@gmail.com>
2024-11-11 01:06:32 -0300

    SolicitudesScreen: Se reemplaza «coroutineScope.launch» por «LaunchedEffect» para manejar la Snackbar.

--- app/src/main/java/com/example/login/ui/screens/solicitudes/SolicitudesScreen.kt
+++ app/src/main/java/com/example/login/ui/screens/solicitudes/SolicitudesScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
@@ -68,7 +69,7 @@ fun SolicitudesScreen(
             }
             is SolicitudesUiState.Error -> {
                 val message = (uiState as SolicitudesUiState.Error).message
-                coroutineScope.launch {
+                LaunchedEffect(snackbarHostState) {
                     snackbarHostState.showSnackbar(
                         message = "Error: $message",
                         actionLabel = "Cerrar"
