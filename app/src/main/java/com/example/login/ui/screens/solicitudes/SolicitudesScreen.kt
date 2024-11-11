051310664323e5bc1cf5c80e7dceb874fc799331
Pehuenia <msaguero@gmail.com>
2024-11-11 00:38:56 -0300

    SolicitudesScreen: Se resuelve desfase entre la aparición del mensaje «No hay solicitudes» y la efectiva confirmación de existencia de las mismas.

--- app/src/main/java/com/example/login/ui/screens/solicitudes/SolicitudesScreen.kt
+++ app/src/main/java/com/example/login/ui/screens/solicitudes/SolicitudesScreen.kt
@@ -1,12 +1,25 @@
 package com.example.login.ui.screens.solicitudes
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.login.R
+import com.example.login.ui.viewmodels.solicitudesviewmod.SolicitudesUiState
 import com.example.login.ui.viewmodels.solicitudesviewmod.SolicitudesViewModel
 import kotlinx.coroutines.launch
 
@@ -16,21 +29,53 @@ fun SolicitudesScreen(
     viewModel: SolicitudesViewModel,
     navController: NavHostController
 ) {
-    val solicitudes by viewModel.solicitudes.observeAsState(emptyList())
-    val error by viewModel.error.observeAsState()
+    val uiState by viewModel.uiState.observeAsState(SolicitudesUiState.Loading)
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
 
-    // Muestra el Snackbar cuando hay un error
-    error?.let {
-        coroutineScope.launch {
-            snackbarHostState.showSnackbar(
-                message = "Error: $it",
-                actionLabel = "Cerrar"
-            )
+
+
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        when (uiState) {
+            is SolicitudesUiState.Loading -> {
+                // Mostrar el indicador de carga mientras los datos se están cargando
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+            is SolicitudesUiState.Success -> {
+                // Mostrar la lista de solicitudes cuando hay datos disponibles
+                val solicitudes = (uiState as SolicitudesUiState.Success).solicitudes
+                SolicitudesLista(solicitudes = solicitudes, navController = navController)
+            }
+            is SolicitudesUiState.Empty -> {
+                // Mostrar el mensaje cuando no hay solicitudes
+                val message = (uiState as SolicitudesUiState.Empty).message
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .offset(y = -100.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = message,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = colorResource(id = R.color.texto_Secundario)
+                    )
+                }
+            }
+            is SolicitudesUiState.Error -> {
+                val message = (uiState as SolicitudesUiState.Error).message
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(
+                        message = "Error: $message",
+                        actionLabel = "Cerrar"
+                    )
+                }
+            }
         }
     }
-
-    SolicitudesLista(solicitudes, navController)
 }
 
