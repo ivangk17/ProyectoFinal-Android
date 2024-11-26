a467cd994c2e8b404461f0c0a9c61268ca3a552c
Pehuenia <msaguero@gmail.com>
2024-11-25 23:16:43 -0300

    Se convierten los objetos estáticos DatosDomicilio, DatosPersonales y DatosPropietarioAfectado en Dataclases, para mejorar la estructura y manejo de datos.

--- app/src/main/java/com/example/login/data/models/personas/DatosPersonales.kt
+++ app/src/main/java/com/example/login/data/models/personas/DatosPersonales.kt
@@ -4,11 +4,10 @@ import androidx.compose.runtime.mutableStateOf
 import com.example.login.data.models.fields.FormField
 import com.example.login.data.models.fields.TipoCampo
 
-object DatosPersonales {
-    val datos = listOf(
-        FormField("Nombre", mutableStateOf(""), tipo = TipoCampo.TEXTO),
-        FormField("Apellido", mutableStateOf(""), tipo = TipoCampo.TEXTO),
-        FormField("CUIT", mutableStateOf(""), tipo = TipoCampo.TEXTO),
-        FormField("Email", mutableStateOf(""), tipo = TipoCampo.TEXTO),
-        FormField("Telefono", mutableStateOf(""), tipo = TipoCampo.TEXTO))
-}
+data class DatosPersonales(
+    var Nombre: String = "",
+    var Apellido: String = "",
+    var CUIT: String = "",
+    var Email: String = "",
+    var Telefono: String = ""
+)
